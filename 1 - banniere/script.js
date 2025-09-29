72bc9ce58e8086bfef6339101de097fd4cea1869
Ralph <radou69@gmail.com>
2025-09-29 14:33:49 +0200

    ajout exo

--- 1 - banniere/script.js
+++ 1 - banniere/script.js
@@ -9,3 +9,14 @@ Nouvelles compétences:
 // 2. Je recupere la banniere de cookies
 
 // 3. J'ecoute l'evenement "click" sur le bouton "Accepter les cookies"
+const banner = document.querySelector(".cookies");
+const btnAccept = document.querySelector(".btn-accept");
+
+btnAccept.addEventListener("click", () => {
+  banner.classList.add("fade-out");
+
+  // Après la durée de la transition (1000 ms), on cache complètement
+  setTimeout(() => {
+    banner.style.display = "none";
+  }, 1000);
+});
